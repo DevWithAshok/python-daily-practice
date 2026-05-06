edc4c4164c80578571dfba33e691d539941f87d4
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-05-06 23:31:55 +0530

    Day 09 - Added complex logical condition programs

---
+++ Day 09 - Complex Conditions/complex_conditions.py
@@ -0,0 +1,23 @@
+M=int(input())
+P=int(input())
+C=int(input())
+rule_1=((M+P)>=100 or (P+C)>=100 or (M+C>=100))
+rule_2=(M+P+C>=180)
+print(rule_1 and rule_2)
+
+A=input()
+first=int(A[0])
+second=int(A[1])
+third=int(A[2])
+result_1=(first>7 and second>7 and third>7)
+result_2=((first*second)<=30 and (second*third)<=30 and (first*third)<=30)
+print(result_1 or result_2)
+
+A=input()
+first=int(A[0])
+second=int(A[1])
+third=int(A[2])
+rule_1=(first==1 or second==1 or third==1)
+rule_2=(first+second+third)<12
+rule_3=(third==5)
+print(rule_1 and rule_2 and rule_3)
