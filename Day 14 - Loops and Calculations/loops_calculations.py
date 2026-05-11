b183af5cb06f2dfa99e78359b142fcd3887bcd89
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-05-11 23:57:21 +0530

    Day 14 - Added loop programs for product and average calculation

---
+++ Day 14 - Loops and Calculations/loops_calculations.py
@@ -0,0 +1,19 @@
+N=int(input())
+counter=0
+product=1
+while counter<N:
+    M=int(input())
+    product=product*M
+    M=M+1
+    counter=counter+1
+print(product)    
+
+
+N=int(input())
+count=0
+sum=0
+for num in range(1,N+1):
+    sum=sum+num
+    count=count+1
+avg=sum/count
+print(avg)
