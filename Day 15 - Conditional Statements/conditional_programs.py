29bd5752caf98a25f3ed5bb21938aac72e363694
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-05-12 23:23:30 +0530

    Day 15 - Added greatest number and leap year programs
    Add a program to find the greatest of three numbers and check for leap year.

---
+++ Day 15 - Conditional Statements/conditional_programs.py
@@ -0,0 +1,17 @@
+A=int(input())
+B=int(input())
+C=int(input())
+if (A>B and A>C):
+    print(A)
+elif(B>A and B>C):
+    print(B)
+else:
+    print(C)
+  //Greatest Among Three Numbers 
+
+Y=int(input())
+cond_1=(Y%400)==0
+cond_2=((Y%4)==0 and (Y%100)!=0)
+print(cond_1 or cond_2)
+
+//Leap year
