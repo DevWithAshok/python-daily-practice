53db88796b3275bbf7a58471931dcf2e716657ef
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-05-13 23:19:10 +0530

    Day 16 - Added pattern printing programs using while loops
    The code now prints patterns based on user input for N and M, with two separate loops for different patterns.

---
+++ Day 16 - Pattern Printing/pattern_printing.py
@@ -0,0 +1,25 @@
+N=int(input())
+counter=0
+sum=0
+while counter<N:
+    sum=int(sum)+1
+    sum=str(sum)
+    counter=counter+1
+    print(sum*N)
+
+
+N=int(input())
+counter=0
+sum=0
+while counter<N:
+    sum=sum+1
+    print("*"*sum)
+    counter=counter+1
+
+
+M=int(input())
+N=int(input())
+counter=0
+while counter<M:
+    print("*"*N)
+    counter=counter+1
