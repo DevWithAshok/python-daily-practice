2e10aabe67c7c8b2c89c54cecb897f911107bf9f
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-05-28 23:19:49 +0530

    Day 30 - Added Python indexing programs

---
+++ Day 30 - Python List Programs/list_indexing.py
@@ -0,0 +1,5 @@
+programming_languages_list = ["Python", "Java", "Ruby", "C", "C++", "Go", "R", "JavaScript", "Swift", "PHP", "Kotlin", "Perl"]
+M=int(input())
+for i in range(M):
+    N=int(input())
+    print(programming_languages_list[N])
