6ead1bc3feb92febd017dc7e955adaced4543931
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-06-04 21:01:57 +0530

    Day 34 - Implement string shuffling based on index list

---
+++ Day 35 - Prime Numbers and Shuffle String/shuffle_string.py
@@ -0,0 +1,9 @@
+def shuffle_string(string, indexes_list):
+    shuf_string=""
+    for i in indices_list:
+        shuf_string+=string[int(i)]
+    return shuf_string
+string = input() #tonyPh
+indices_list = input().split() #4 3 0 5 1 2
+result = shuffle_string(string,indices_list)
+print(result)
