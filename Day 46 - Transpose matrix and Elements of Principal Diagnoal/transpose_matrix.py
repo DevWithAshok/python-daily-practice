6b7e5db3ce2512e2d9994ccdfe21d056d8374e41
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-06-15 21:51:16 +0530

    Day 46 - Add function to transpose a matrix
    Implement matrix transposition from user input.

---
+++ Day 46 - Transpose matrix and Elements of Principal Diagnoal/transpose_matrix.py
@@ -0,0 +1,21 @@
+def convert_string_to_int(list_a):
+    new_list = []
+    for item in list_a:
+        num = int(item)
+        new_list.append(num)
+    return new_list
+m, n = input().split() #3 3
+# 1 2 3
+# 10 20 30
+# 5 10 15
+m, n = int(m), int(n)
+num_list = []
+for i in range(m):
+    list_a = input().split()
+    list_a = convert_string_to_int(list_a)
+    num_list.append(list_a)
+for i in range(n):
+    row=[]
+    for j in range(m):
+        row+=[num_list[j][i]]
+    print(row)
