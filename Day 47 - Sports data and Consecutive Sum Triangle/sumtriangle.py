8089084737c6a45e05afbc3e0ecd1df463242764
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-06-16 23:38:45 +0530

    Add sumtriangle.py for consecutive sum calculation

---
+++ Day 47 - Sports data and Consecutive Sum Triangle/sumtriangle.py
@@ -0,0 +1,21 @@
+def conv_str_to_int(N):
+    new=[]
+    for num in N:
+        new+=[int(num)]
+    return new
+def cons_num(value):
+    lis_t=[]
+    length=len(value)-1
+    for i in range(length):
+        lis_t+=[value[i]+value[i+1]]
+    length-=1
+    return lis_t
+N=input().split(",") # 3,5,7,9
+value=conv_str_to_int(N)
+for i in range(len(N)):
+    if i==0:
+        print(value)
+    else:
+        result=cons_num(value)
+        value=result
+        print(result)
