c951a9466cbc0517c76e338b37f12e8b913ab48c
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-06-17 13:30:47 +0530

    Day 47 - Add anti-diagonal extraction for matrix input
    Implement functionality to print anti-diagonals of a matrix.

---
+++ Day 48 - Shift Numbers and Anti Diagonal/antidiagonals.py
@@ -0,0 +1,11 @@
+M,N=map(int,input().split()) # 2 3
+# 1 5 5
+# 2 7 8
+diag_matrix_a=[list(map(int,input().split()))for _ in range(M)]
+for i in range((M+N)-1):
+    new=[]
+    for j in range(M):
+        check=i-j
+        if 0<=check<N:
+            new.append(diag_matrix_a[j][check])
+    print(*new)
