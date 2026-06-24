d1d6b4ffd823192774c1b445e81994da63e5fab7
Ashok Kumar Reddy Chinnavula <ashok052606@gmail.com>
2026-06-24 20:51:49 +0530

    Day 53 - Add functionality to print future date after years

---
+++ Day 54 - Print Dates and Add Years/print_dates.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timedelta
+D1=input() #Jul 11 2014
+# 5
+Y=int(input())
+date_string="%b %d %Y"
+date_object=datetime.strptime(D1,date_string)
+date_object=date_object+timedelta(days=365*Y)
+print(date_object)
